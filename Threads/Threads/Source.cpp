42dfd56d36192088bc208f0b0770f3fb3d19d3ba
Rabia Arif <rabia.arif@infotechgroup.com>
2023-02-23 11:35:16 +0500

    Source.cpp: 2 threads running same function...........Source1.cpp: 2 thread running same function having mutex

---
+++ Threads/Threads/Source.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<thread>
+//
+//void f1(int num)
+//{
+//	for (int i = 0; i < 10; i++)
+//	{
+//		std::cout << "\n Running thread " << num << " for " << i << " th time.";
+//	}
+//}
+//
+//int main()
+//{
+//	std::thread t1(f1, 1);
+//	std::thread t2(f1, 2);
+//	
+//	/*t1.join();
+//
+//	t2.join();*/
+//	
+//	/*t1.detach();
+//
+//	t2.detach();*/
+//
+//	std::cout << "foo: " << t1.joinable() << '\n';
+//	std::cout << "bar: " << t2.joinable() << '\n';
+//
+//	if (t1.joinable()) t1.join();
+//	if (t2.joinable()) t2.join();
+//
+//	/*std::cout << t1.get_id() << std::endl;
+//	std::cout << t2.get_id() << std::endl;*/
+//	
+//
+//	std::cout << "\n\n\n";
+//	return 0;
+//}
